0fdd8dba519078bbc675dd723e8d535570c875f3
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-06-13 20:21:31 -0400

    Add data - item and alternative for each category

---
+++ data.js
@@ -0,0 +1,22 @@
+exports.householdItem = [
+  {title: 'Non-stick pan', category: 'Kitchen', alternatives: 'insert' },
+  {title: 'Shampoo', category: 'Bathroom', alternatives: 'insert'},
+  {title: 'Clothes', category: 'Bedroom', alternatives: 'insert'},
+  {title: 'Pencil', category: 'Workspace', alternatives: 'insert'},
+  {title: 'Candles', category: 'Living room', alternatives: ''},
+];
+
+exports.ecoAlternative = [
+  {content: 'Cast iron', description: 'insert'},
+  {content: 'Stainless steel', description: 'insert'},
+  {content: 'Enameled cast iron', description: 'insert'},
+  {content: 'Refill', description: 'Bring your own container to refill at stores'},
+  {content: 'Bar', description: 'Package-free'},
+  {content: 'Buy in bulk', description: 'Buying in larger sizes reduces the amount of packaging'},
+  {content: 'Terracycle Program', description: 'Allows you to dispose of the waste responsibly'},
+  {content: 'Second-hand', description: 'Thrift shopping, Hand-me downs, etc.'},
+  {content: 'Natural fabrics', description: 'Avoid synthetic fabrics that shed microplastics when washed (e.g. polyester, acrylic, nylon). Choose cotton, linen, wool, cashmere, silk, hemp, bamboo, leather. '},
+	{content: 'Wooden pencil', description: 'insert'},
+	{content: 'Beeswax candles', description: 'Naturally purify the air'},
+	{content: 'Soy wax candles', description: 'insert'}
+];

29e7b92d101004d3a41018c2904e0885a4b73936
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-06-13 20:41:26 -0400

    Consider all alternatives for 5 items

--- data.js
+++ data.js
@@ -1,22 +1,30 @@
 exports.householdItem = [
-  {title: 'Non-stick pan', category: 'Kitchen', alternatives: 'insert' },
-  {title: 'Shampoo', category: 'Bathroom', alternatives: 'insert'},
-  {title: 'Clothes', category: 'Bedroom', alternatives: 'insert'},
-  {title: 'Pencil', category: 'Workspace', alternatives: 'insert'},
-  {title: 'Candles', category: 'Living room', alternatives: ''},
+  {title: 'Non-stick, teflon pan', category: 'Kitchen', alternatives: 'insert' }, //1
+  {title: 'Shampoo', category: 'Bathroom', alternatives: 'insert'}, //2
+  {title: 'Clothing', category: 'Bedroom', alternatives: 'insert'}, //3
+  {title: 'Pencil', category: 'Workspace', alternatives: 'insert'}, //4
+	{title: 'Candle', category: 'Living room', alternatives: ''}, //5
 ];
+/*Want to include for sure:
+- Dish sponge: 
+- Hair removal: sugaring, razor (stainless steel or Terracycle), epilator, 
+ */
 
 exports.ecoAlternative = [
-  {content: 'Cast iron', description: 'insert'},
-  {content: 'Stainless steel', description: 'insert'},
-  {content: 'Enameled cast iron', description: 'insert'},
-  {content: 'Refill', description: 'Bring your own container to refill at stores'},
-  {content: 'Bar', description: 'Package-free'},
-  {content: 'Buy in bulk', description: 'Buying in larger sizes reduces the amount of packaging'},
-  {content: 'Terracycle Program', description: 'Allows you to dispose of the waste responsibly'},
-  {content: 'Second-hand', description: 'Thrift shopping, Hand-me downs, etc.'},
-  {content: 'Natural fabrics', description: 'Avoid synthetic fabrics that shed microplastics when washed (e.g. polyester, acrylic, nylon). Choose cotton, linen, wool, cashmere, silk, hemp, bamboo, leather. '},
-	{content: 'Wooden pencil', description: 'insert'},
-	{content: 'Beeswax candles', description: 'Naturally purify the air'},
-	{content: 'Soy wax candles', description: 'insert'}
+  {content: 'Cast iron', description: 'insert'}, //1
+  {content: 'Stainless steel', description: 'insert'}, //1
+  {content: 'Enameled cast iron', description: 'insert'}, //1
+  {content: 'Refill', description: 'Bring your own container to refill at stores or buy pre-packaged in a glass jar where you can return it.'}, //2
+  {content: 'Bar', description: 'Package-free, solid bar'}, //2
+  {content: 'Buy in bulk', description: 'Buying in larger sizes reduces the amount of packaging'}, //2
+	{content: 'Terracycle Program', description: 'Provides programs for consumers to dispose of their waste responsibly.'}, //2 //4
+	//Terracycle --> pens, skincare products, razors, Brita filter, detergent, electronics, cleaning
+  {content: 'Second-hand', description: 'Thrift shops, Hand-me downs, etc.'}, //3
+  {content: 'Natural fabrics', description: 'Avoid synthetic fabrics that shed microplastics when washed (e.g. polyester, acrylic, nylon). Choose cotton, linen, wool, cashmere, silk, hemp, bamboo, leather. '}, //3
+	{content: 'Repair the clothes', description: 'Mend, darn, felt, hem, or patch up the piece of clothing instead of discarding it.'}, //3
+	{content: 'Borrow the clothes', description: 'Borrow a piece of clothing from a family/friend if you\'re only going to wear it once.'}, //3
+	{content: 'Wooden pencil', description: 'Wood is compostable.'}, //4
+	{content: 'Stainless steel mechanical pencil', description: 'Stainless steel is a high-value material and can be reused.'}, //4
+	{content: 'Beeswax candles', description: 'Naturally purify the air'}, //5
+	{content: 'Soy wax candles', description: 'insert'} //5
 ];

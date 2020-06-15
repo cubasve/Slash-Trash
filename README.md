5b9f93887ad9ca8770d0d8634bb3899bcf5b8270
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-06-15 17:47:34 -0400

    Update read.me - next steps

--- README.md
+++ README.md
@@ -28,3 +28,16 @@ STATE WHAT THE PROBLEM IS FIRST: Why should we avoid plastic?
 ## Getting Started:
 
 ## Next Steps: Planned Future Enhancements
+-  ## Include a page for beginners:
+Users who are novices to this idea might feel overwhelmed with all the information.
+A link that includes beginner swaps (e.g. using a reusable water bottle or grocery bag) would be beneficial.
+
+- ## Include a page for Tips:
+An additional resource to guide novice and/or experienced users on their journey to low-waste living.
+Incorporating a page that provides tips and tricks would be great.
+
+- ## Incorporate a feedback link: 
+Allows the user to input their opinions about the application. This allows for the continual improvement of the application. 
+
+- ## No results found for a search:
+There are many products in a household and it is difficult to compile them all into the application. By adding a feature where I can see what items are not returning results, I can add that particular item into the data model for future users who want to search for that same item.
--- data.js
+++ data.js
@@ -3,14 +3,13 @@
     --> I have to put variables for ecoAlternatives (ex. castIronID) before householdItems
 - ReferenceError: Cannot access 'uuidv4' before initialization
     --> const { v4: uuidv4} = require('uuid') has to be at the top of the page
--ReferenceError: ObjectId is not defined
+- ReferenceError: ObjectId is not defined
     --> ObjectId is for mongoose model (data.js is a JS file, not mongoose file)--> I said the id type would be a string
     --> uuidv4() function returns a string 
-
 */
 
 const { v4: uuidv4 } = require('uuid'); //Creates random strings of ObjectIds for items & alternatives
-// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' --> string is returned from function
+// uuidv4(); ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' --> string is returned from function
 
 //create variable from uuid() holds value of uuid
 const castIronID = uuidv4(); 
@@ -30,89 +29,75 @@ const ssMechPencilID = uuidv4();
 module.exports = { //array (ecoAlternative)--> objects (each swap) --> key:value pairs (id, swap, description)
   ecoAlternative: [
     {
-      // _id: ObjectId(castIronID), // Teflon/non-stick pan
-      _id: castIronID,
+      _id: castIronID, //Teflon, non-stick pan
       //search by field instead of id
       swap: 'Cast Iron', 
       description: '', 
     }, //1
     {
-      // _id: ObjectId(stainlessSteelID),
       _id: stainlessSteelID,
       swap: 'Stainless Steel', 
       description: '',
     }, //1
       //NEXT STEP: {swap: 'Stainless steel ${householdItem.title}} --> shows 'Stainless steel pan'
     {
-      // _id: ObjectId(enameledCastIronID),
       _id: enameledCastIronID,
       swap: 'Enameled Cast Iron', 
       description: '', 
     }, //1
     {
-      // _id: ObjectId(refillID),
       _id: refillID,
       swap: 'Refill', 
       description: 'Bring your own container to refill at stores or buy pre-packaged in a glass jar where you can return it.', 
     }, //2
     {
-      // _id: ObjectId(barID),
       _id: barID,
       swap: 'Bar', 
       description: 'Package-free, solid bar', 
     }, //2
     {
-      // _id: ObjectId(bulkID),
       _id: bulkID,
       swap: 'Buy in Bulk', 
       description: 'Buying in larger sizes reduces the amount of packaging', 
     }, //2
     {
-      // _id: ObjectId(terracycleID),
       _id: terracycleID,
       swap: 'Terracycle Program', 
       description: 'Provides programs for consumers to dispose of their waste responsibly.', 
     }, //2 //4
     //Terracycle --> pens, skincare products, razors, Brita filter, detergent, electronics, cleaning
     {
-      // _id: ObjectId(secondHandID),
       _id: secondHandID,
       swap: 'Second-Hand', 
       description: 'Thrift shops, Hand-me downs, etc.', 
     }, //3
     {
-      // _id: ObjectId(naturalFabricID),
       _id: naturalFabricID,
       swap: 'Natural Fabrics', 
       description: 'Avoid synthetic fabrics that shed microplastics when washed (e.g. polyester, acrylic, nylon). Choose cotton, linen, wool, cashmere, silk, hemp, bamboo, leather. ', 
     }, //3 //5
     {
-      // _id: ObjectId(repairID),
       _id: repairID,
       swap: 'Repair', 
       description: 'Mend, darn, felt, hem, or patch up the piece of fabric instead of discarding it.', 
     }, //3 //5
     {
-      // _id: ObjectId(borrowID),
       _id: borrowID,
       swap: 'Borrow', 
       description: 'Borrow from a family member/friend if you\'re only going to wear it once.', 
     }, //3
     {
-      // _id: ObjectId(woodenPencilID),
       _id: woodenPencilID,
       swap: 'Wooden Pencil', 
       description: 'Wood is compostable.', 
     }, //4
     {
-      // _id: ObjectId(ssMechPencilID),
       _id: ssMechPencilID,
       swap: 'Stainless Steel Mechanical Pencil', 
       description: 'Stainless steel is a high-value material. There is a high chance it will be recycled and reused.', 
     }, //4
   ]
 }
-//NOTE: nodemon says ObjectId 
 
 /*
 EXAMPLE FROM MONGODB LECTURE
@@ -162,9 +147,6 @@ module.exports = { //object - array - object
         castIronID,
         stainlessSteelID,
         enameledCastIronID,
-        // ObjectId(castIronID), //reference string instead of 'iro'
-        // ObjectId(stainlessSteelID), 
-        // ObjectId(enameledCastIronID),
       ],
     }, //1
     {
@@ -176,10 +158,6 @@ module.exports = { //object - array - object
         barID,
         bulkID,
         terracycleID,
-        // ObjectId(refillID), 
-        // ObjectId(barID), 
-        // ObjectId(bulkID),
-        // ObjectId(terracycleID),
       ], 
     },
     {
@@ -191,10 +169,6 @@ module.exports = { //object - array - object
         naturalFabricID,
         repairID,
         borrowID,
-        // ObjectId(secondHandID), 
-        // ObjectId(naturalFabricID), 
-        // ObjectId(repairID),
-        // ObjectId(borrowID),
       ], 
     }, //3
     {
@@ -204,8 +178,6 @@ module.exports = { //object - array - object
       alternatives: [
         woodenPencilID,
         ssMechPencilID,
-        // ObjectId(woodenPencilID), 
-        // ObjectId(ssMechPencilID), 
       ], 
     }, //4
     {
@@ -216,9 +188,6 @@ module.exports = { //object - array - object
         naturalFabricID,
         repairID,
         secondHandID,
-        // ObjectId(naturalFabricID), 
-        // ObjectId(repairID), 
-        // ObjectId(secondHandID)
       ], 
     }, //5
   ]
@@ -235,11 +204,3 @@ module.exports = { //object - array - object
  MATERIALS TO FOCUS ON:
  - Bamboo, stainless steel, glass, paper/wood, aluminum, silicone, cotton, ceramic, metal, copper, cork, linen, plant fibers, stoneware, clay
  */
-
-// module.exports = {
-//   getAll,
-// }
-
-// function getAll(){
-//   return ecoAlternative;
-// }

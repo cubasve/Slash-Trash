b7f353045cebc5f01e61902e078d11ca27bc2387
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-06-18 12:14:16 -0400

    swap list

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 
 module.exports = {
 	listSwap,
+	showSwap,
 	createSwap,
 	deleteSwap,
 };
@@ -14,11 +15,25 @@ module.exports = {
 //4. attach user document to req.user --> REQ.USER = LOGGED IN USER VIA OAUTH
 //swaps are now attached to logged in user
 function listSwap(req, res) {
-	res.render('users', { user: req.user });
+	if (!req.user) {
+		return res.redirect('/');
+	}
+	User.findById(req.user._id).populate('swapsForUser.item').populate('swapsForUser.alternative').exec(function (err, user) {
+		console.log(user);
+		res.render('users', { user: user });
+	})
 	//name of EJS file - not /swaps, /users
-	//
 	//render a view
+}
 
+function showSwap(req, res) {
+	if (!req.user) {
+		return res.redirect('/');
+	}
+	User.findById(req.user._id).populate('swapsForUser.item').populate('swapsForUser.alternative').exec(function (err, user) {
+		console.log(user);
+		res.render('/showSwaps', { user: user });
+	})
 }
 
 function createSwap(req, res) {
